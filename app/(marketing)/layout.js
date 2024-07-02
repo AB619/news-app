d8b39dc5dea79cfa3bfabc20de0ed83cef75e106
Ankit Bhaskar <ankitkumar619619@gmail.com>
2024-07-02 23:11:48 +0530

    [develop] route group added

---
+++ app/(marketing)/layout.js
@@ -0,0 +1,15 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "News App",
+  description:
+    "Learn how to route to different pages. Next.js Page Routing & Rendering",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
